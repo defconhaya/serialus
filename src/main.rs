3a898195a96157c9b8e2c683db4ceffa70a1ab3a
Haya <razvan.olteanu@nortonlifelock.com>
2023-11-30 17:12:43 +0200

    chat window

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,97 @@
-use std::io::{stdout, Write};
-use crossterm::cursor::{MoveTo};
-use crossterm::{QueueableCommand};
+use crossterm::cursor::MoveTo;
+use crossterm::event::{poll, read, Event, KeyCode, KeyModifiers};
 use crossterm::terminal::{self, Clear, ClearType};
-use  std::time::Duration;
-use  std::thread;
+use crossterm::QueueableCommand;
+use std::io::{stdout, Stdout, Write, Bytes};
+use std::ops::{Mul, Sub};
+use std::thread;
+use std::time::Duration;
+
+struct Rect {
+    x: usize,
+    y: usize,
+    w: usize,
+    h: usize,
+}
+
+fn chat_window(stdout: &mut impl Write, chat: &[String], boundary: Rect) {
+    let n = chat.len();
+    let m = n.checked_sub(boundary.h as usize).unwrap_or(0);
+    for (row, line) in chat.iter().skip(m).enumerate() {
+        stdout
+            .queue(MoveTo(boundary.x as u16, (boundary.y + row) as u16))
+            .unwrap();
+        let bytes = line.as_bytes();
+        stdout.write(bytes.get(0..boundary.w).unwrap_or(bytes)).unwrap();
+    }
+}
 
 // use serial2::SerialPort;
 // use std::env;
 
+// fn chat_window(stdout: &mut impl QueueableCommand)
 
-fn main()  {
+fn main() {
     let mut stdout = stdout();
-    let (width, height) = terminal::size().unwrap();
-    stdout.queue(Clear(ClearType::All)).unwrap();
-    let label = b"alupigus";
-    stdout.queue(MoveTo(width/2-label.len() as u16/2, height/2)).unwrap();
-    stdout.write(label).unwrap();
-    
-    stdout.flush().unwrap();
-    thread::sleep(Duration::from_secs(5));
+    terminal::enable_raw_mode().unwrap();
+    let (mut w, mut h) = terminal::size().unwrap();
+    let bar_char = "─";
+    let mut bar = bar_char.repeat(w as usize);
+    let mut prompt = String::new();
+    let mut chat = Vec::new();
+    let mut quit = false;
+    while !quit {
+        while poll(Duration::ZERO).unwrap() {
+            match read().unwrap() {
+                Event::Resize(nw, nh) => {
+                    w = nw;
+                    h = nh;
+                    bar = bar_char.repeat(w as usize);
+                }
+                Event::Paste(data) =>{
+                    prompt.push_str(&data);
+                }
+                Event::Key(event) => match event.code {
+                    KeyCode::Char(x) => {
+                        if x == 'c' && event.modifiers.contains(KeyModifiers::CONTROL) {
+                            quit = true;
+                        }
+                        prompt.push(x)
+                    }
+                    KeyCode::Esc => quit = true,
+                    KeyCode::Enter => {
+                        chat.push(prompt.clone());
+                        prompt.clear();
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
+        stdout.queue(Clear(ClearType::All)).unwrap();
+        chat_window(
+            &mut stdout,
+            &chat,
+            Rect {
+                x: 0,
+                y: 0,
+                w: w as usize,
+                h: h as usize- 2,
+            },
+        );
+
+        stdout.queue(MoveTo(0, h - 2)).unwrap();
+        stdout.write(bar.as_bytes()).unwrap();
+        stdout.queue(MoveTo(1, h - 1)).unwrap();
+        {
+            let bytes = prompt.as_bytes();
+            stdout.write(bytes.get(0..w as usize).unwrap_or(bytes)).unwrap();
+        }
+        stdout.write(prompt.as_bytes()).unwrap();
+        stdout.flush();
+        thread::sleep(Duration::from_millis(33));
+        terminal::disable_raw_mode().unwrap();
+    }
 }
 
 // fn test_ser() -> Result<(), ()> {
